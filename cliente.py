e4517b1ebcfe2b89ed0f8608dac6d6c7dd338e07
LeandroYara <l.yara@uniandes.edu.co>
2022-10-22 10:50:52 -0500

    Comunicación dirigida

--- cliente.py
+++ cliente.py
@@ -2,29 +2,32 @@ import socket
 import select
 
 UDP_IP = "127.0.0.1"
+UDP_SR = "127.0.0.1"
 IN_PORT = 5005
-ADDR = (UDP_IP, IN_PORT)
+ADDRC = (UDP_IP, IN_PORT)
+ADDRS = (UDP_SR, IN_PORT)
 SIZE = 1024
-timeout = 3
-
+timeout = 1
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind((UDP_IP, IN_PORT))
+
+sock.sendto("Nueva solicitud", ADDRS)
+data, addr = sock.recvfrom(1024)
+fileName = data[0]
+numeroCliente = data[1]
+
+print ("Se establecio la comunicación con el servidor")
+print("IP y puerto del servidor: " + addr)
+print ("Nombre del archivo por recibir: ", fileName)
+
+file = open(fileName, 'wb')
 
 while True:
-    data, addr = sock.recvfrom(1024)
-    if data:
-        print ("File name:", data)
-        file_name = data.strip()
-
-    f = open(file_name, 'wb')
-
-    while True:
-        ready = select.select([sock], [], [], timeout)
-        if ready[0]:
-            data, addr = sock.recvfrom(1024)
-            f.write(data)
-        else:
-            print ("%s Finish!" % file_name)
-            f.close()
-            break
+    ready = select.select([sock], [], [], timeout)
+    if ready[0]:
+        data, addr = sock.recvfrom(1024)
+        file.write(data)
+    else:
+        print ("%s Terminado!" % fileName)
+        file.close()
+        break
--- servidor.py
+++ servidor.py
@@ -1,3 +1,4 @@
+import os
 import socket
 import time
 import sys
@@ -6,27 +7,34 @@ UDP_IP = "127.0.0.1"
 UDP_PORT = 5005
 ADDR = (UDP_IP, UDP_PORT)
 SIZE = 1024
+NCL = 1
 
 tipo = input("Ingrese 1 si quiere enviar el archivo de 100MB o ingrese 2 si quiere enviar el archivo de 250MB: ")
 
 if tipo == "1":
-    file = open("ArchivosEnvio/100MB.bin", "rb")
+    filePath = "ArchivosEnvio/100MB.bin"
 elif tipo == "2":
-    file = open("ArchivosEnvio/250MB.bin", "rb")
-
-file_name = sys.argv[1]
-
+    filePath = "ArchivosEnvio/250MB.bin"
+    
+fileName = os.path.basename(filePath)
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind(ADDR)
 
 while (True):
-    sock.sendto(file_name, ADDR)
-    print ("Sending %s ..." % file_name)
-
-    file = open("ArchivosEnvio/" + file_name, "r")
+    
+    mensaje, addrc = sock.recvfrom(SIZE)
+    data = [fileName, NCL]
+    NCL += 1
+    print(addrc + ": " + mensaje)
+    
+    sock.sendto(data, addrc)
+    
+    print ("Enviando %s ..." % fileName)
+    file = open("ArchivosEnvio/" + fileName, "r")
     data = file.read(SIZE)
     while(data):
-        if(sock.sendto(data, ADDR)):
+        if(sock.sendto(data, addrc)):
             data = file.read(SIZE)
             time.sleep(0.02) # Give receiver a bit time to save
+    file.close()
